856360f6acd0693e261610a14d1eb81320dac842
Johnson <2534426042@qq.com>
2017-09-26 14:29:04 +0800

    minor changes

--- functions.php
+++ functions.php
@@ -4,13 +4,13 @@ if( !function_exists('write_log') ) {
 
     // 记录日志
     function write_log($message) {
-        $file = ROOT_PATH . 'storage\\logs\\' . date('Ymd');
+        $file = ROOT_PATH . 'storage/logs/' . date('Ymd');
 
         if( !is_dir($file) ) {
             mkdir($file);
         }
 
-        file_put_contents($file . '\\info.log', $message, FILE_APPEND | LOCK_EX);
+        file_put_contents($file . '/info.log', $message, FILE_APPEND | LOCK_EX);
 
     }
 }
--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
-    define('ROOT_PATH', __DIR__ . '\\');
-    require ROOT_PATH . 'config\\config.php';
+    define('ROOT_PATH', __DIR__ . '/');
+    require ROOT_PATH . 'config/config.php';
     require ROOT_PATH . 'functions.php';
 
     class WebSocketServer {
